bb01c4f529b2c724d5b122a14bb374be41181fa8
Gabriel Metola <metolag@gmail.com>
2024-11-23 09:02:19 +0100

    [PRIMO-INVISIBLE] Esto estaba sin commitear desde a saber cuándo.

--- amigo_invisible.py
+++ amigo_invisible.py
@@ -1,24 +1,72 @@
+import ast
 import pandas as pd
 import random
 
-datafr = pd.read_csv(r'C:\Users\metol\Documents\amigo_invisible.csv',sep=';')
-datafr = datafr.sort_values(by='dificultad',ascending=False)
-datafr = datafr.reset_index(drop=True)
+global TABLA_BASE
 
+def convert_to_list(s):
+    return ast.literal_eval(s)
 
-# People should not have as invisible friend their partner or siblings
-people = datafr['nombre']
-ya_tiene = set()
-for index in range(len(datafr)):
-    posibles = set(people) - set(eval(datafr['Exclusiones'][index])) - ya_tiene
-    sorteo = random.sample(posibles,1)[0]
-    ya_tiene.add(sorteo)
-    datafr.at[index, 'Regala a'] = sorteo
+def initialize(last_year):
+    tabla = TABLA_BASE
+    tabla = tabla.sort_values(by='dificultad',ascending=False)
+    tabla = tabla.reset_index(drop=True)
 
-print(datafr[datafr['Regala a']!='Gabriel'])
+    # People should not have as invisible friend their partner or siblings
+    people = tabla['nombre']
+    ya_tiene = set()
+    return tabla, people, ya_tiene
 
-print("Espera a Sara")
+def exclude_last_year(tabla, last_results):
+    for index in range(len(last_results)):
+        try:
+            king = last_results['nombre'][index]
+            peasant = last_results['Regala a'][index]
+            king_index = tabla.index[tabla['nombre'] == king].tolist()[0]
+            tabla.loc[king_index, 'Exclusiones'].append(peasant)
+            print(f"Este año {king} no regala a {peasant}")
+        except:
+            print(f"Algo ha pasado entre {king} y {peasant}. Seguramente alguno de los dos no regala este año o no regaló el pasado.")
+            continue
+    return tabla
 
-print(datafr[datafr['Regala a']=='Gabriel'])
+converters = {'Exclusiones': convert_to_list}
+original = pd.read_csv(r'C:\Users\metol\Documents\amigo_invisible\amigo_invisible.csv',sep=';', converters=converters)
+last_year = pd.read_csv(r'C:\Users\metol\Documents\amigo_invisible\resultado_amigo_invisible_2022.csv',sep=';', converters=converters)
+TABLA_BASE = pd.read_csv(r'C:\Users\metol\Documents\amigo_invisible\amigo_invisible_2023.csv',sep=';', converters=converters)
+TABLA_BASE = exclude_last_year(TABLA_BASE, last_year)
+
+tabla, people, ya_tiene = initialize(last_year)
+
+print("Personas que faltan con respecto al original: ", set(original['nombre']) - set(people))
+print("Personas que no estaban en el original: ", set(people) - set(original['nombre']))
+tries = 0
+
+while True:
+    try:
+        for index in range(len(tabla)):
+            posibles = set(people) - set(tabla['Exclusiones'][index]) - ya_tiene
+            sorteo = random.sample(posibles,1)[0]
+            ya_tiene.add(sorteo)
+            tabla.at[index, 'Regala a'] = sorteo
+        break
+    except:
+        print("Con esta combinación, alguien se quedaba sin regalo.")
+        tabla, people, ya_tiene = initialize(last_year)
+        tries += 1
+        if tries < 20:
+            continue
+        else:
+            print("No doy con la combinación perfecta...")
+            exit()
+
+print(tabla[tabla['Regala a']!='Gabriel'])
+
+print("Va a salir el nombre de la persona que regala a Gabriel.")
+
+print(tabla[tabla['Regala a']=='Gabriel'])
+
+print("Guardando el resultado para evitar repetirlo el año que viene.")
+tabla.to_csv(r'C:\Users\metol\Documents\amigo_invisible\resultado_amigo_invisible_2023.csv',sep=';')
 
 print("Bye")
